503490015ee3ce8123edfb0a7858f2c1d4f59d3f
weiyi <weiyi@ruijie.com.cn>
2026-04-30 16:33:33 +0800

    Release v0.1.6

--- lib/core/app_version.dart
+++ lib/core/app_version.dart
@@ -1,7 +1,7 @@
 class AppVersion {
   const AppVersion._();
 
-  static const String version = '0.1.5';
-  static const String buildNumber = '6';
+  static const String version = '0.1.6';
+  static const String buildNumber = '7';
   static const String display = 'v$version+$buildNumber';
 }
