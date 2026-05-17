a612b7657fb4cf9f58e2be835e281781d97784d0
wyn <wyn@wyndeMacBook-Pro.local>
2026-05-17 14:43:45 +0800

    Release v0.1.9

--- lib/core/app_version.dart
+++ lib/core/app_version.dart
@@ -1,7 +1,7 @@
 class AppVersion {
   const AppVersion._();
 
-  static const String version = '0.1.8';
-  static const String buildNumber = '9';
+  static const String version = '0.1.9';
+  static const String buildNumber = '10';
   static const String display = 'v$version+$buildNumber';
 }
